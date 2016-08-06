8545bc8f9b1ea52eaae29dba9289baa3f9b5c627
Jack <jackg7@vt.edu>
2016-08-06 14:21:03 -0400

    added dependencies and basic server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require('./static/app/index')
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var neo4j = require('neo4j-driver').v1;
+var app = express();
+
+app.set('port', (process.env.PORT | 3000));
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
